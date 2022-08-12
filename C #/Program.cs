cacfbdcf8ff84cec258649d930557bdedebf80f9
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-08-12 23:13:26 +0530

    ASSIGNMENT (4.8.22)(A METHOD TO ACCEPT 3 SUB MARKS AND PRINT TOTAL,PER,GRADE)

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,45 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pro_30_emp_method
+namespace Pro_31_total_per_grade
 {
-    class Employee
-    {
-        int empno;
-        String empname;
-        float salary;
-        
-        public void getdata(int empno, String empname, float salary)
-        {
-            this.empno = empno;
-            this.empname = empname;
-            this.salary = salary;
-
-        }
-        public void displaydata()
-        {
-            Console.WriteLine("empno " + empno);
-            Console.WriteLine("empname " + empname);
-            Console.WriteLine("salary " + salary);
-        }
-
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Employee emp = new Employee();
-            Console.WriteLine("enter employee no");
-
-            int eno = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter employee name");
-            String en = Console.ReadLine();
-            Console.WriteLine("enter employee salary");
-            float sal = Convert.ToSingle(Console.ReadLine());
-
-            emp.getdata(eno, en, sal);
-            emp.displaydata();
-
+            double s1, s2, s3, total, per;
+            string name, grd;
+            Console.WriteLine("Calculate the toal,percentage and grade");
+            Console.WriteLine("Enter the name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the sub1 marks");
+            s1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the sub2 marks");
+            s2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the sub3 marks");
+            s3 = Convert.ToInt32(Console.ReadLine());
+            total = s1 + s2 + s3;
+            per = total / 3.0;
+            if (per >= 75)
+                grd = "Distinction";
+            else if (per < 75 && per >= 60)
+                grd = "First";
+            else if (per < 60 && per >= 45)
+                grd = "Second";
+            else
+                grd = "Fail";
+            Console.WriteLine("\nName of Student=" + name);
+            Console.Write("Total Marks={0}\n Percentage={1} \n Grade={2}", total, per, grd);
             Console.ReadLine();
         }
     }
