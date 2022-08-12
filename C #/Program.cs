4a40fe32407d0d220a3f0d3c7a0e75dc6fecfda0
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-08-12 21:48:46 +0530

    GREATER NUMBER

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,20 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Enterlongnumber
+namespace Greater_no
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            long b1;
-            Console.WriteLine("enter long number  ");
-            b1 = Convert.ToInt64(Console.ReadLine());
-            Console.WriteLine("long entered = " + b1);
-
+            int a, b, c;
+            Console.WriteLine("Enter no 1");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter no 2");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter no 3");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a > b && a > c)
+                Console.WriteLine("a is greater");
+            else if (b > a && b > c)
+                Console.WriteLine("b is greater");
+            else
+                Console.WriteLine("c is greater");
             Console.ReadLine();
-
-
         }
     }
 }
