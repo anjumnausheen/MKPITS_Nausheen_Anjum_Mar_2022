35795ee1b904b34e46987b76862ccf83027ec500
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-08-12 21:34:19 +0530

    EVEN_ODD

---
+++ C #/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("entrer a no");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 0)
+                Console.WriteLine("no is even");
+            else
+                Console.WriteLine("no is odd");
+            Console.ReadLine();
+        }
+    }
+}
