7725bdb9ee8ee25fc4a25ac6bc4339b7eaef8970
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-08-12 21:55:54 +0530

    PRODUCT_NAME_RATE_QUANTITY_BONUS

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,28 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cal_dis_bonus
+namespace Pro_name_rate_quan_total
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string empname, desig;
-            int empno;
+            float prate, total, dis;
+            int quan;
+            string pname;
 
-            Console.WriteLine("enter emp no");
-            empno = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter emp name");
-            empname = Console.ReadLine();
-            Console.WriteLine("enter designation");
-            desig = Console.ReadLine();
-
-            if (desig == "manager")
-                Console.WriteLine("bonus=" + 10000);
-            else if (desig == "clerk")
-                Console.WriteLine("bonus=" + 5000);
-            else if (desig == "peon")
-                Console.WriteLine("bonus=" + 2000);
+            Console.WriteLine("enter product rate");
+            prate = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("enter product name");
+            pname = Console.ReadLine();
+            Console.WriteLine("enter quantity");
+            quan = Convert.ToInt32(Console.ReadLine());
+            total = prate * quan;
+            Console.WriteLine("total=" + total);
+            if (total > 2000)
+                dis = total * 0.50f;
+            else
+                dis = total * 0.10f;
+            total = prate * quan;
+            Console.WriteLine("discount=" + dis);
             Console.ReadLine();
         }
     }
