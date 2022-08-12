4e30afed385c9c3819b5121de283af6223109e75
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-08-12 22:31:50 +0530

    PROGRAM_14_DOUBLE

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pro_11_short_pos_no
+namespace Pro_14_double
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            ushort b1;
-            Console.WriteLine("enter ushort number ");
-            b1 = Convert.ToUInt16(Console.ReadLine());
-            Console.WriteLine("ushort entered = " + b1);
+            double b1;
+            Console.WriteLine("enter double number ");
+            b1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("double entered = " + b1);
 
             Console.ReadLine();
         }
