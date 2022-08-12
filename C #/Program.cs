d25c25649c95531272e8b340ce3d2725e44f4beb
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-08-12 22:22:20 +0530

    PROGRAM_7_CHAR_USER

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,19 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Program_6_radius
+namespace Program_7_char_user
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int radius;
-            Console.WriteLine("enter radius");
-            radius = Convert.ToInt32(Console.ReadLine());
-            float area = 3.14f * radius * radius;
-            Console.WriteLine("area = " + area);
-            float circ = 2 * 3.14f * radius;
-            Console.WriteLine("circ = " + circ);
+            char ch;
+            Console.WriteLine("enter character ");
+            ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("character entered = " + ch);
+
             Console.ReadLine();
         }
     }
