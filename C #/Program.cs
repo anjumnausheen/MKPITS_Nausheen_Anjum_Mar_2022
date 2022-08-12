0c64b2642b94e4fc3cfff7ea59f763c9054e5509
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-08-12 22:45:54 +0530

    PROGRAM_23_FOR_EACH

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pro_22_array_5_element
+namespace Pro_23_for_each
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] num = new int[3];
-            num[0] = 33;
-            num[1] = 22;
-            num[2] = 31;
-            //using foreach loop to iterate over the elements of an array
-            foreach (int i in num)
+            string[] num = new string[3];
+            num[0] = "amit";
+            num[1] = "raj";
+            num[2] = "dilip";
+            
+            foreach (string i in num)
             {
                 Console.WriteLine(i);
             }
