042c1f439a0e346d20cb1946d418adb6d6b87b7b
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-08-13 00:20:39 +0530

    ASSIGNMENT(9.8.22)(EMPLOYEE_WITH_CONSTRUCTOR_AND_PARAMETER)

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,77 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Class_Student_with_Method_Overloading
+namespace Employee_with_Constructor_and_parameter
 {
-    class Student
+    class Employee
     {
-        int rn;
-        string name, course;
-        int fees;
+        int empno;
+        string empname;
+        string desig;
+        int salary;
 
-        public void getdata()
+        //creating a  constructor with no parameters
+        public Employee()
         {
-            rn = 9;
-            name = "neelu";
-            course = "python";
-            fees = 8000;
+            empno = 1;
+            empname = "Rani";
+            desig = "teacher";
+            salary = 50000;
         }
-        // method overloaded
-        public void getdata(int rn, string name)
+
+
+        //creating a  parameterized constructor with 4 parameters
+        public Employee(int empno, string empname, string desig, int salary)
         {
-            this.rn = rn;
-            this.name = name;
-        }
-        public void getdata(int rn, string name, string course)
-        {
-            this.rn = rn;
-            this.name = name;
-            this.course = course;
-        }
-        public void getdata(int rn, string name, string course, int fees)
-        {
-            this.rn = rn;
-            this.name = name;
-            this.course = course;
-            this.fees = fees;
+            Console.WriteLine("Constructor with parameter called");
+            this.empno = empno;
+            this.empname = empname;
+            this.desig = desig;
+            this.salary = salary;
         }
         public void display()
         {
-            Console.WriteLine("student roll no = " + rn);
-            Console.WriteLine("student name = " + name);
-            Console.WriteLine("course= " + course);
-            Console.WriteLine("fees= " + fees);
-
+            Console.WriteLine("emp no=" + empno);
+            Console.WriteLine("emp name=" + empname);
+            Console.WriteLine("emp designation=" + desig);
+            Console.WriteLine("emp salary=" + salary);
         }
+
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Student s = new Student();
-            s.getdata();
-            s.display();
+            Employee e = new Employee(); //this will call constructor automatically
+            e.display();
 
-            Console.WriteLine("------------------------");
+            Console.WriteLine("--------employee 1 details -------");
+            Employee e1 = new Employee(5, "meena", "Peon", 10000);// this will call parameterized constructor
+            e1.display();
 
+            Console.WriteLine("-----------------Employee 2 details ----------");
+            Console.WriteLine("enter emp no");
+            int emp1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter emp name");
+            String nam1 = Console.ReadLine();
+            Console.WriteLine("enter emp designation");
+            String desi1 = Console.ReadLine();
+            Console.WriteLine("enter emp salary");
+            int sal1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("------------------------");
-            Console.WriteLine("student  details");
-            Student s2 = new Student();
-            s2.getdata(123, "ayesha");
-            s2.display();
-            Console.WriteLine("------------------------");
-            Console.WriteLine("Student  details");
-            Student s3 = new Student();
-            s3.getdata(123, "ayesha", "java");
-            s3.display();
-            Console.WriteLine("------------------------");
-            Console.WriteLine("student  details");
-            Student s4 = new Student();
-            s4.getdata(123, "ayesha", "java", 10000);
-            s4.display();
-
-
+            Employee e2 = new Employee(emp1, nam1, desi1, sal1);// this will call parameterized constructor
+            e2.display();
 
             Console.ReadLine();
         }
