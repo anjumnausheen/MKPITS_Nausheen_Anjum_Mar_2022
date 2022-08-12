5bbe79d6f09ee4bd71bc2aa686f5de50e63c82c8
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-08-12 22:14:33 +0530

    PROGRAM

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,26 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Prime_not_prime
+namespace program1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int i, n;
-            Console.WriteLine("enter a no");
-            n = Convert.ToInt32(Console.ReadLine());
-            for (i = 2; i < n; i++)
-            {
-                if (n % i == 0)
-                {
-                    Console.WriteLine(" Not Prime no");
-
-                    break;
-                }
-            }
-            if (i == n)
-                Console.WriteLine("Prime no");
+            int l, b;
+            Console.WriteLine("enter length");
+            l = Convert.ToInt32(Console.ReadLine()); //toint32 method will convert string to integer
+            Console.WriteLine("enter breadth");
+            b = Convert.ToInt32(Console.ReadLine());
+            int result = l * b;
+            Console.WriteLine("Area of Rectangle=" + result);
             Console.ReadLine();
         }
     }
