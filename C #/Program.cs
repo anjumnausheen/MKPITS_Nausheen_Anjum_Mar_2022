97f70a6f91a8cdae2e999528ca9525122aaab3eb
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-09-18 20:16:45 +0530

    Add files via upload

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,68 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employee_with_Constructor_and_parameter
+namespace Hello_world
 {
-    class Employee
-    {
-        int empno;
-        string empname;
-        string desig;
-        int salary;
-
-        //creating a  constructor with no parameters
-        public Employee()
-        {
-            empno = 1;
-            empname = "Rani";
-            desig = "teacher";
-            salary = 50000;
-        }
-
-
-        //creating a  parameterized constructor with 4 parameters
-        public Employee(int empno, string empname, string desig, int salary)
-        {
-            Console.WriteLine("Constructor with parameter called");
-            this.empno = empno;
-            this.empname = empname;
-            this.desig = desig;
-            this.salary = salary;
-        }
-        public void display()
-        {
-            Console.WriteLine("emp no=" + empno);
-            Console.WriteLine("emp name=" + empname);
-            Console.WriteLine("emp designation=" + desig);
-            Console.WriteLine("emp salary=" + salary);
-        }
-
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Employee e = new Employee(); //this will call constructor automatically
-            e.display();
-
-            Console.WriteLine("--------employee 1 details -------");
-            Employee e1 = new Employee(5, "meena", "Peon", 10000);// this will call parameterized constructor
-            e1.display();
-
-            Console.WriteLine("-----------------Employee 2 details ----------");
-            Console.WriteLine("enter emp no");
-            int emp1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter emp name");
-            String nam1 = Console.ReadLine();
-            Console.WriteLine("enter emp designation");
-            String desi1 = Console.ReadLine();
-            Console.WriteLine("enter emp salary");
-            int sal1 = Convert.ToInt32(Console.ReadLine());
-
-            Employee e2 = new Employee(emp1, nam1, desi1, sal1);// this will call parameterized constructor
-            e2.display();
-
+            int x, y, z;
+            x = 10;
+            y = 20;
+            z = x + y;
+            Console.WriteLine("Addition=" + z);
             Console.ReadLine();
+
         }
     }
 }
