66b5851233e726bd1e0a56894b9ecfa0e6a4aadf
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-09-19 23:44:15 +0530

    Pro to check no. is even or odd

--- Program.cs
+++ Program.cs
@@ -4,24 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Program_If3
+namespace Program_even_odd
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int x, y;
-            Console.WriteLine("Enter 2 number");
-            x = Convert.ToInt32(Console.ReadLine());
-            y = Convert.ToInt32(Console.ReadLine());
-            if (x>y)
+            int x;
+             Console.WriteLine("Enter any number:");
+            x =Convert.ToInt32 (Console.ReadLine());
+            if (x % 2 == 0)
             {
-                Console.WriteLine("x is greater");
+                Console.WriteLine("Number is even:");
             }
             else
             {
-                Console.WriteLine("y is greater");
+                Console.WriteLine("Number is odd");
             }
+
             Console.ReadLine();
         }
     }
