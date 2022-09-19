9c32bcfb9237f3a433dcebd84a800e692b3ac068
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-09-19 23:22:53 +0530

    Unary Increment Operator

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,18 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Subtraction
+namespace Unary_Increment_operator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int x, y, z;
-            Console.WriteLine("Enter 2 no");
-            x = Convert.ToInt32(Console.ReadLine());
-            y = Convert.ToInt32(Console.ReadLine());
-            z = x - y;
-            Console.WriteLine("Subtraction =" + z);
+            int x=2 , y;
+            y = +x;
+            Console.WriteLine(" x=" + x + " y=" + y);
             Console.ReadLine();
         }
     }
