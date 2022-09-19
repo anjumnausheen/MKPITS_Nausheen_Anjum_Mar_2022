a2325340ee13c4d7ee2d703725d7b70c280ef4e3
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-09-19 23:53:17 +0530

    Pro to input & check , alphabet , digit or special character

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,35 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Program_vowel_consonant
+namespace Program_alpabet_digit_special_character
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             char ch;
-            Console.WriteLine("Enter any character");
+            Console.WriteLine("Enter any character:");
             ch = Convert.ToChar(Console.ReadLine());
-            if(ch >= 'A' && ch <= 'Z' || (ch >= 'a' && ch<='z'))
+            if(ch>='A' && ch<='Z' || ch>='a' && ch<='z' )
             {
-                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-                {
-                    Console.WriteLine("alphabet is vowel");
-                }
-                else if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-                {
-                    Console.WriteLine("alphabet is vowel");
-                }
-                else
-                {
-                    Console.WriteLine("alphabet is consonant");
-                }
+                Console.WriteLine("Character is alphabet");
+            }
+            else if(ch>='0' && ch<='9')
+            {
+                Console.WriteLine("Character is number");
             }
             else
             {
-                Console.WriteLine("Character is not alphabet");
+                Console.WriteLine("Character is special character");
             }
-            
             Console.ReadLine();
         }
     }
