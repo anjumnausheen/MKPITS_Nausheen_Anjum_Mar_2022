e237eb6cd24b9258399b8c10856bad928f79a68d
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-09-19 23:45:37 +0530

    Pro to check a no. is divisible by 5 & 11 or not

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,27 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Program_neg_pos
+namespace Program_divisible_notdivisible
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int n;
+            int x;
             Console.WriteLine("Enter a number:");
-            n = Convert.ToInt32(Console.ReadLine());
-            if(n>0)
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x % 5 == 0)
             {
-                Console.WriteLine("Number is positive");
-            }
-            else if (n < 0)
-            {
-                Console.WriteLine("Number is negative");
+                Console.WriteLine("Number is  divisible by 5 ");
             }
             else
             {
-                Console.WriteLine("Number is Zero");
+                Console.WriteLine("Number is not divisible by 5 ");
             }
+            if (x % 11 == 0) 
+            {
+                Console.WriteLine("Number is divisible by 11");
+            }
+            else
+            {
+                Console.WriteLine("Number is not divisible by 11");
+            }
+
             Console.ReadLine();
         }
     }
