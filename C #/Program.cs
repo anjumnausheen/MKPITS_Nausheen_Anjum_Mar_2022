4b07e3941a85bfff7524f8c775a45a5e909dd96e
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-09-19 23:59:20 +0530

    Pro to input month no. & print no. of days in that month

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Program_weekno_weekday
+namespace Program_monthno_no_of_days
 {
     internal class Program
     {
@@ -14,36 +14,55 @@ namespace Program_weekno_weekday
             Console.WriteLine("Enter any number:");
             x = Convert.ToInt32(Console.ReadLine());
 
-            if ( x==1)
+            if(x == 1) 
             {
-                Console.WriteLine("Sunday");
+                Console.WriteLine("January 31 days");
             }
-            else if( x==2)
+            else if(x == 2) 
             {
-                Console.WriteLine("Monday");
+                Console.WriteLine("February 28 days");
             }
-            else if (x == 3)
+            else if(x == 3)
             {
-                Console.WriteLine("Tuesday");
+                Console.WriteLine("March 31 days");
             }
             else if(x == 4)
             {
-                Console.WriteLine("wednesday");
+                Console.WriteLine("April 30 days");
             }
             else if(x == 5)
             {
-                Console.WriteLine("Thursday");
-
+                Console.WriteLine("May 31 days");
             }
             else if(x == 6)
             {
-                Console.WriteLine("Friday");
+                Console.WriteLine("June 30 days");
             }
             else if(x == 7)
             {
-                Console.WriteLine("Saturday");
+                Console.WriteLine("July 31 days");
             }
-            else
+            else if(x == 8)
+            {
+                Console.WriteLine("August 31 days");
+            }
+            else if(x == 9)
+            {
+                Console.WriteLine("September 30 days ");
+            }
+            else if( x == 10)
+            {
+                Console.WriteLine("October 31 days");
+            }
+            else if(x == 11)
+            {
+                Console.WriteLine("November 30 days");
+            }
+            else if(x == 12)
+            {
+                Console.WriteLine("December 31 days");
+            }
+            else 
             {
                 Console.WriteLine("Invalid Input");
             }
