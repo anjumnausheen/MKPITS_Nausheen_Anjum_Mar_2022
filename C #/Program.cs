0df048f18818792bc2955a92d98c12626cf315d3
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-09-19 23:13:44 +0530

    Unary pre increment

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unary_operator
+namespace Unary_pre_increment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             int x = 2, y;
-            y = x++;
-            Console.WriteLine("x=" + x + "  y=" + y);
+            y = ++x;
+            Console.WriteLine("x=" + x + " y=" + y);
             Console.ReadLine();
         }
     }
