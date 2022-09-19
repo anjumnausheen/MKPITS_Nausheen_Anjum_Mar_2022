0dbcd0cd1c769cf7eb875a5cc4f7a266d9933de8
anjumnausheen <102462049+anjumnausheen@users.noreply.github.com>
2022-09-19 23:08:40 +0530

    Module operator

--- C #/Program.cs
+++ C #/Program.cs
@@ -4,19 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Relational_operator
+namespace Module_operator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a = 17, b = 7;
-            Console.WriteLine(a > b);
-            Console.WriteLine(a < b);
-            Console.WriteLine(a >= b);
-            Console.WriteLine(a <= b);
-            Console.WriteLine(a == b);
-            Console.WriteLine(a != b);
+            int x = 17, y = 7;
+            int z = x % y;
+            Console.WriteLine(z);
             Console.ReadLine();
         }
     }
